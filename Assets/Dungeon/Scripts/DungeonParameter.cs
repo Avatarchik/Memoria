99490f6cbfe92c80881cd6f5279f5b6f1120ca42
MAX-eipi <psp.dsi.wii@gmail.com>
2015-07-16 19:50:42 +0900

    DungeonParameter内のパラメータ追加 ・getKeyNum ・allKeyNum

--- Assets/Dungeon/Scripts/DungeonParameter.cs
+++ Assets/Dungeon/Scripts/DungeonParameter.cs
@@ -21,20 +21,32 @@ namespace Memoria.Dungeon
             get { return _sp; }
             set { _sp = Mathf.Clamp(value, 0, maxSp); }
         }
-
+        
+        public int floor { get; set; }
+        
         public int maxSp { get; set; }
 
-        public int floor { get; set; }
+        private int _getKeyNum;
 
+        public int getKeyNum
+        {
+            get { return _getKeyNum; }
+            set { _getKeyNum = Mathf.Clamp(value, 0, allKeyNum); }
+        }
+
+        public int allKeyNum { get; set; }
+        
         private string skill { get; set; }
 
-        public DungeonParameter(int maxHp, int hp, int maxSp, int sp, int floor, string skill)
+        public DungeonParameter(int maxHp, int hp, int maxSp, int sp, int floor, int allKeyNum, string skill)
         {
             this.maxHp = maxHp;
             this.hp = hp;
             this.maxSp = maxSp;
             this.sp = sp;
             this.floor = floor;
+            this.allKeyNum = allKeyNum;
+            this.getKeyNum = 0;
             this.skill = skill;
         }
     }
