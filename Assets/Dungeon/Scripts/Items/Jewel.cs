3735d277f9cf39c0d84117dd4cc3976ffbc4119f
MAX-eipi <psp.dsi.wii@gmail.com>
2015-07-17 12:23:38 +0900

    Jewelコンポーネントの作成

---
+++ Assets/Dungeon/Scripts/Items/Jewel.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using Memoria.Dungeon.BlockComponent;
+
+namespace Memoria.Dungeon.Items
+{
+    public struct JewelData
+    {
+        public BlockType blockType { get; set; }
+        public Vector2Int location { get; set; }
+    }
+
+    public class Jewel : MonoBehaviour
+    {
+        public JewelData jewelData { get; set; }
+        
+        // Use this for initialization
+        //  void Start()
+        //  {
+            
+        //  }
+
+        // Update is called once per frame
+        //  void Update()
+        //  {
+
+        //  }
+    }
+}
