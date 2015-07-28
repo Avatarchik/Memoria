b67be4485c379291ab736a9461e34511d2667325
MAX-eipi <psp.dsi.wii@gmail.com>
2015-07-28 23:48:47 +0900

    TipsViewerの仮動作を実装

--- Assets/Dungeon/Scripts/Menu/TipsViewer.cs
+++ Assets/Dungeon/Scripts/Menu/TipsViewer.cs
@@ -1,18 +1,38 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using UniRx;
+using UniRx.Triggers;
+using Memoria.Dungeon.Managers;
 
-public class TipsViewer : MonoBehaviour
+namespace Memoria.Dungeon.Menu
 {
-
-    // Use this for initialization
-    void Start()
+    public class TipsViewer : MonoBehaviour
     {
+        [SerializeField]
+        private Button viewTipButton;
+        
+        [SerializeField]
+        private Animator tipsViewerAnimator;
 
-    }
+        // Use this for initialization
+        void Start()
+        {
+            viewTipButton.OnClickAsObservable()
+                .Subscribe(_ =>
+                {
+                    DungeonManager.instance.EnterState(DungeonState.OpenMenu/*TipsViewer*/);
+                    tipsViewerAnimator.SetBool("show", true);
+                    DungeonManager.instance.ExitState();
+                });
+        }
 
-    // Update is called once per frame
-    void Update()
-    {
+        // Update is called once per frame
+        //  void Update()
+        //  {
+
+        //  }
+
 
     }
 }
