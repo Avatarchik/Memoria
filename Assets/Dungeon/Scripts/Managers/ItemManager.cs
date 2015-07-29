14ccab753e4b449c0bec757e7e4dd367e2e68a3d
MAX-eipi <psp.dsi.wii@gmail.com>
2015-07-29 23:17:23 +0900

    ItemManagerの作成

---
+++ Assets/Dungeon/Scripts/Managers/ItemManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Memoria.Dungeon.Managers
+{
+    public class ItemManager : MonoBehaviour
+    {
+        public static ItemManager instance { get { return null; } }
+        
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
