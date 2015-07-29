6386bcfee91d18eceddc257dd147377978e1e5ce
MAX-eipi <psp.dsi.wii@gmail.com>
2015-07-29 23:53:30 +0900

    ItemTaking Stateの追加

--- Assets/Dungeon/Scripts/Managers/DungeonManager.cs
+++ Assets/Dungeon/Scripts/Managers/DungeonManager.cs
@@ -11,6 +11,7 @@ namespace Memoria.Dungeon.Managers
         BlockOperating,
         PlayerMoving,
         BlockEvent,
+        ItemTaking,
 		StockTaking,
         OpenMenu,
         LeaveSelect,
