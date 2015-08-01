b1a3e3a83cbc0caec19cc028a7445f23bfd39776
MAX-eipi <psp.dsi.wii@gmail.com>
2015-08-01 14:30:53 +0900

    購読処理のリファクタリング パワーストックのパラメータ変更処理の追加

--- Assets/Dungeon/Scripts/Managers/ParameterManager.cs
+++ Assets/Dungeon/Scripts/Managers/ParameterManager.cs
@@ -1,14 +1,26 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
+using System.Collections.Generic;
 using System.Linq;
 using UniRx;
 using Memoria.Dungeon.Items;
+using Memoria.Dungeon.BlockComponent;
+using Memoria.Battle.GameActors;
 
 namespace Memoria.Dungeon.Managers
 {
     public class ParameterManager : MonoBehaviour
     {
         public static ParameterManager instance { get { return DungeonManager.instance.parameterManager; } }
+
+        private static Dictionary<BlockType, int> toIndex =
+            new Dictionary<BlockType, int>()
+            {
+                { BlockType.Thunder, 0 },
+                { BlockType.Wind, 1 },
+                { BlockType.Water, 2 },
+                { BlockType.Fire, 3 },
+            };
 
         private ReactiveProperty<DungeonParameter> _parameter = new ReactiveProperty<DungeonParameter>();
 
@@ -17,7 +29,7 @@ namespace Memoria.Dungeon.Managers
             get { return _parameter.Value; }
             set { _parameter.Value = value; }
         }
-        
+
         public IObservable<DungeonParameter> OnChangeParameterAsObservable()
         {
             return _parameter.AsObservable();
@@ -35,13 +47,20 @@ namespace Memoria.Dungeon.Managers
         [SerializeField]
         private Text sillingText;
 
+        [SerializeField]
+        private ElementalPowerStock[] charactersPowerStocks = new ElementalPowerStock[4];
+
         public void Awake()
         {
-            var dungeonManager = DungeonManager.instance;
-            var blockManager = BlockManager.instance;
-            var mapManager = MapManager.instance;
-            var itemManager = ItemManager.instance;
-            var parameterChanged = _parameter.AsObservable();
+            SubscribeParameterChangeEvent();
+            SubscribePlayerWalkEvent();
+            SubscribeBlockOpertateEvent();
+            SubscribeItemEvent();
+        }
+
+        private void SubscribeParameterChangeEvent()
+        {
+            var parameterChanged = OnChangeParameterAsObservable();
 
             // HPの変化イベントの追加
             parameterChanged
@@ -74,15 +93,23 @@ namespace Memoria.Dungeon.Managers
             parameterChanged
                 .DistinctUntilChanged(param => param.silling)
                 .Subscribe(UpdateSillingText);
+        }
 
+        private void SubscribePlayerWalkEvent()
+        {
             // プレイヤーが歩き終わったあと
-            dungeonManager.player.OnWalkEndAsObservable()
+            DungeonManager.instance.player.OnWalkEndAsObservable()
                 .Subscribe(_ =>
                 {
                     var param = parameter;
                     param.sp -= 1;
                     parameter = param;
                 });
+        }
+
+        private void SubscribeBlockOpertateEvent()
+        {
+            var blockManager = BlockManager.instance;
 
             // ブロックの破壊時
             blockManager.OnCreateBlockAsObservable()
@@ -102,8 +129,11 @@ namespace Memoria.Dungeon.Managers
                     param.sp -= 2;
                     parameter = param;
                 });
+        }
 
-            var createItem = itemManager.OnCreateItemAsObservable();
+        private void SubscribeItemEvent()
+        {
+            var createItem = ItemManager.instance.OnCreateItemAsObservable();
 
             // キーを取得した時
             createItem
@@ -119,13 +149,39 @@ namespace Memoria.Dungeon.Managers
             // 宝石を取得した時
             createItem
                 .Where(item => item.itemData.type == ItemType.Jewel)
-                .SelectMany(item => item.OnTakeAsObservable())
-                .Subscribe(_ => 
+                .SelectMany(item => item.OnTakeAsObservable().Select(_ => item.itemData.attribute))
+                .Subscribe(attribute =>
                 {
+                    TakePowerStock(attribute);
+
                     var param = parameter;
                     param.silling += 1000;
-                    parameter = param; 
+                    parameter = param;
                 });
+
+            // 魂を取得したとき
+            createItem
+                .Where(item => item.itemData.type == ItemType.Soul)
+                .SelectMany(item => item.OnTakeAsObservable().Select(_ => item.itemData.attribute))
+                .Subscribe(FillPowerStock);
+        }
+
+        public void TakePowerStock(BlockType attribute)
+        {
+            int index = toIndex[attribute];
+            int nowCount = parameter.stocks[index];
+            int fillCount = 3;
+            int nextCount = Mathf.Min(nowCount + 1, fillCount);
+            parameter.stocks[index] = nextCount;
+            charactersPowerStocks[index].stock = nextCount;
+        }
+
+        public void FillPowerStock(BlockType attribute)
+        {
+            int index = toIndex[attribute];
+            int fillCount = 3;
+            parameter.stocks[index] = fillCount;
+            charactersPowerStocks[index].stock = fillCount;
         }
 
         private void UpdateHpText(DungeonParameter parameter)
