49a4cb6589328ed78e370284eb4dee1bf3bb85e4
MAX-eipi <psp.dsi.wii@gmail.com>
2015-08-13 12:49:30 +0900

    NumberSpriteコンポーネントの作成

---
+++ Assets/Dungeon/Scripts/NumberSprite.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Memoria.Dungeon
+{
+    public class NumberSprite : MonoBehaviour
+    {
+        [SerializeField]
+        private int _value = 0;
+
+        public int value
+        {
+            get
+            {
+                return _value;
+            }
+
+            set
+            {
+                _value = value;
+                int num = value;
+
+                for (int i = 0; i < digits.Length; i++)
+                {
+                    digits[i].SetFloat("value", num % 10);
+                    num /= 10;
+                }
+            }
+        }
+
+        [SerializeField]
+        private Animator[] digits;
+
+        //  // Use this for initialization
+        //  void Start()
+        //  {
+
+        //  }
+
+        //  // Update is called once per frame
+        //  void Update()
+        //  {
+
+        //  }
+    }
+}
