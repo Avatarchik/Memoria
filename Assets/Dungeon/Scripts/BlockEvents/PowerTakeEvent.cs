26e9edde2c0e3de342685ce095c30a3baa7153ac
MAX-eipi <psp.dsi.wii@gmail.com>
2015-08-15 06:09:17 +0900

    回復イベント時のエフェクト追加

--- Assets/Dungeon/Scripts/BlockEvents/PowerTakeEvent.cs
+++ Assets/Dungeon/Scripts/BlockEvents/PowerTakeEvent.cs
@@ -71,6 +71,11 @@ namespace Memoria.Dungeon.BlockEvents
             eventAnimator.SetFloat("eventType", 1);
             EventManager.instance.message = "ＨＰ回復！！";
             eventAnimator.SetTrigger("getPower");
+            
+            var effectPosition = Camera.main.transform.position;
+            effectPosition.z = 0;
+            EffectManager.instance.InstantiateEffect(12, effectPosition, 2f);
+
             yield return new WaitForSeconds(1);
         }
     }
