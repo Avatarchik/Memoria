8a7caff0cccdcf81cc605d51c807cdb6ca54997e
MAX-eipi <psp.dsi.wii@gmail.com>
2015-08-15 22:48:04 +0900

    ButtonTapEffectコンポーネントの作成

---
+++ Assets/Dungeon/Scripts/Effect/ButtonTapEffect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Memoria.Dungeon.Effect
+{
+    public class ButtonTapEffect : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
