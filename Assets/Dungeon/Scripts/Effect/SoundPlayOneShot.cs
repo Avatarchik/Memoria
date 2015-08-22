32580be9eba9a0d9a8b8c6efd7791e615aa1267d
MAX-eipi <psp.dsi.wii@gmail.com>
2015-08-22 11:30:03 +0900

    SoundPlayOneShot スクリプトの作成

---
+++ Assets/Dungeon/Scripts/Effect/SoundPlayOneShot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Memoria.Dungeon.Effect
+{
+    public class SoundPlayOneShot : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
