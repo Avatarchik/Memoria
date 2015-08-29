cefd7d37df4b6ae9131ea40eb6af997ce1292291
MAX-eipi <psp.dsi.wii@gmail.com>
2015-08-29 13:51:16 +0900

    silling : int の追加

--- Assets/Data/GameData.cs
+++ Assets/Data/GameData.cs
@@ -5,5 +5,19 @@ namespace Memoria
 {
     public static class GameData
     {
+        public static int silling
+        {
+            get
+            {
+                if (!PlayerPrefs.HasKey("silling"))
+                {
+                    PlayerPrefs.SetInt("silling", 0);
+                }
+
+                return PlayerPrefs.GetInt("silling");
+            }
+            
+            set { PlayerPrefs.SetInt("silling", value); }
+        }
     }
 }
