446f45cd244e4cc3e2ef1992d1fae86b516c1094
MAX-eipi <psp.dsi.wii@gmail.com>
2015-09-15 12:10:34 +0900

    Reset スクリプトの作成

---
+++ Assets/Title/Scripts/Reset.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Memoria.Title
+{
+    public class Reset : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
